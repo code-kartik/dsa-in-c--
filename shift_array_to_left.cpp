fc3d9de4fac9b826dd85513c169657b88f82158e
Kartik Arora <kartikarora23082003@gmail.com>
2024-06-24 10:49:36 +0530

    shift array by k places

--- shift_array_to_left.cpp
+++ shift_array_to_left.cpp
@@ -3,21 +3,39 @@
 #include <limits.h>
 using namespace std;
 
+vector<int> shiftToLeftByKPlaces(vector<int> v, int k)
+{
+
+    k = k % v.size();
+
+    vector<int> temp;
+
+    for (int i = 0; i < k; i++)
+    {
+        temp.push_back(v[i]);
+    }
+
+    for (int i = k; i < v.size(); i++)
+    {
+        v[i - k] = v[i];
+    }
+
+    for (int i = v.size() - k; i < v.size(); i++)
+    {
+        v[i] = temp[i - (v.size() - k)];
+    }
+
+    return v;
+}
+
 int main()
 {
     // time complexity: O(N)
     vector<int> arr{1, 2, 3, 4, 5};
 
-    int temp = arr[0];
+    vector<int> result = shiftToLeftByKPlaces(arr, 3);
 
-    for (int i = 1; i < arr.size(); i++)
-    {
-        arr[i - 1] = arr[i];
-    }
-
-    arr[arr.size() - 1] = temp;
-
-    for (auto i : arr)
+    for (auto i : result)
     {
         cout << i << " ";
     }
